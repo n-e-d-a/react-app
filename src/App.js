79c3234215265096c013541ad2646773858d4d79
n-e-d-a <87589027+n-e-d-a@users.noreply.github.com>
2021-10-16 17:39:49 +0330

    remove empty title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     
     <div className="weather-box">
-      <h1></h1>
+      
       <Weather city="paris" />
       <a href="https://github.com/wecodeschool/react-weather">Open-Source Code</a> by Neda Zare
     </div>
